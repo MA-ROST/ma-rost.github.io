bf436fd8861d852cd7b123c83e8d05137ad42971
Marissa R.S. <mrowles@academic.rrc.ca>
2024-08-14 19:07:29 -0500

    Update Index and Javascript
    Hopefully this will fix it not functioning on Pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,3 @@
-// Import all of Bootstrap's JS
-import * as bootstrap from 'bootstrap'
-import Alert from 'bootstrap/js/dist/alert'
-
-// or, specify which plugins you need:
-import { Tooltip, Toast, Popover } from 'bootstrap'
+//
+// Place any custom JS here
+//
